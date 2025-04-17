bbf9dc196d561ae2a2fc3a513294edec46022a26
Andrew <andrew.neeley@icloud.com>
2025-04-17 14:42:40 -0400

    Fetch notes from the DB

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,8 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, boolean } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+export const notes = pgTable('notes', {
+    id: serial('id').primaryKey(),
+    content: text('content').notNull(),
+    markdown: boolean('markdown').notNull().default(false),
+    encrypted: boolean('encrypted').notNull().default(false),
 });
---
+++ src/routes/[slug]/+page.server.ts
@@ -0,0 +1,20 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { notes } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
+
+export const load: PageServerLoad = async ({ params }) => {
+    let noteIndex = parseInt(params.slug, 36) - 10000
+    const result = await db.select({
+        content: notes.content,
+        encrypted: notes.encrypted,
+        markdown: notes.markdown
+    }).from(notes).where(eq(notes.id, noteIndex))
+
+    if (result.length > 0) {
+        return result[0]
+    }
+
+    throw error(404, 'Note not found');
+};

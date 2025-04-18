a288dd7cd3271364666c63623b8a954b8cfe1ef7
Andrew <andrew.neeley@icloud.com>
2025-04-18 17:01:47 -0400

    Add link to sourcecode

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -5,3 +5,4 @@ export { default as Edit } from './Edit.svelte';
 export { default as Eye } from './Eye.svelte';
 export { default as Code } from './Code.svelte';
 export { default as Page } from './Page.svelte';
+export { default as Github } from './Github.svelte';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Github } from '$lib/icons';
 	import '../app.css';
 
 	let { children } = $props();
@@ -40,33 +41,40 @@
 	class="bg-base-300 flex h-14 w-full flex-row items-center justify-between px-5 max-sm:absolute max-sm:bottom-0 md:px-10 xl:px-20"
 >
 	<h1 class="ui">Quicknote</h1>
-	<div class="dropdown dropdown-end max-sm:dropdown-top">
-		<div tabindex="-1" role="button" class="btn m-1">
-			Theme
-			<svg
-				width="12px"
-				height="12px"
-				class="inline-block h-2 w-2 fill-current opacity-60"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 2048 2048"
-			>
-				<path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-			</svg>
+	<div class="flex flex-row items-center space-x-6">
+		<button
+			class="btn btn-ghost transition-all hover:opacity-50"
+			onclick={() => window.open('https://github.com/aaneeley/quicknote', '_blank')}
+			><Github /></button
+		>
+		<div class="dropdown dropdown-end max-sm:dropdown-top">
+			<div tabindex="-1" role="button" class="btn m-1">
+				Theme
+				<svg
+					width="12px"
+					height="12px"
+					class="inline-block h-2 w-2 fill-current opacity-60"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 2048 2048"
+				>
+					<path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+				</svg>
+			</div>
+			<ul tabindex="-1" class="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
+				{#each allowed_themes as theme}
+					<li>
+						<input
+							data-key="theme"
+							type="radio"
+							name="theme-dropdown"
+							class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+							aria-label={theme.friendlyName}
+							data-set-theme={theme.value}
+						/>
+					</li>
+				{/each}
+			</ul>
 		</div>
-		<ul tabindex="-1" class="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
-			{#each allowed_themes as theme}
-				<li>
-					<input
-						data-key="theme"
-						type="radio"
-						name="theme-dropdown"
-						class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-						aria-label={theme.friendlyName}
-						data-set-theme={theme.value}
-					/>
-				</li>
-			{/each}
-		</ul>
 	</div>
 </div>
 <main class="px-5 md:px-10 xl:px-20">

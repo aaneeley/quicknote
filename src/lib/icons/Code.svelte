5a8e58a3295459e22764e91f920e7d9ea144b33b
Andrew <andrew.neeley@icloud.com>
2025-04-18 03:23:42 -0400

    Add icons for raw view

---
+++ src/lib/icons/Code.svelte
@@ -0,0 +1,11 @@
+<svg
+	class="swap-off"
+	fill="currentColor"
+	xmlns="http://www.w3.org/2000/svg"
+	width="20"
+	height="20"
+	viewBox="0 -960 960 960"
+	><path
+		d="M320-240 80-480l240-240 57 57-184 184 183 183zm320 0-57-57 184-184-183-183 56-56 240 240z"
+	/></svg
+>
---
+++ src/lib/icons/Page.svelte
@@ -0,0 +1,11 @@
+<svg
+	class="swap-on"
+	xmlns="http://www.w3.org/2000/svg"
+	width="20"
+	height="20"
+	viewBox="0 -960 960 960"
+	fill="currentColor"
+	><path
+		d="M240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80zm280-520v-200H240v640h480v-440zM240-800v200zv640z"
+	/></svg
+>
